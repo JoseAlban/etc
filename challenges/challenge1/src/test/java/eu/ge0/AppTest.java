00e39f43a0278fbc8b07aff73eeb3480c239de24
Jose Alban <zebaum@gmail.com>
2015-09-07 00:51:02 +0100

    test challenge1

--- challenges/challenge1/src/test/java/eu/ge0/AppTest.java
+++ challenges/challenge1/src/test/java/eu/ge0/AppTest.java
@@ -2,11 +2,49 @@ package eu.ge0;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class AppTest {
 
+    int[] haystack, needle;
+
     @Test
-    public void itDoesThings() throws Exception {
+    public void itReturnsPositionOfSecondArrayWithinFirst() throws Exception {
+        haystack = new int[]{2, 3, 4, 5};
+        needle = new int[]{4, 5};
 
+        assertEquals(2, App.findPosition(haystack, needle));
+    }
 
+    @Test
+    public void itReturnsNegativeNumberWhenNeedleNotFoundInHaystack() throws Exception {
+        haystack = new int[]{2, 3, 4, 5};
+        needle = new int[]{4, 7};
+
+        assertEquals(Integer.MIN_VALUE, App.findPosition(haystack, needle));
+    }
+
+    @Test
+    public void itReturnsNegativeNumberWhenNeedleIsLargerThanHaystack() throws Exception {
+        haystack = new int[]{1, 2, 3};
+        needle = new int[]{1, 2, 3, 4};
+
+        assertEquals(Integer.MIN_VALUE, App.findPosition(haystack, needle));
+    }
+
+    @Test
+    public void itReturnsNegativeNumberWhenNeedleIsEmpty() throws Exception {
+        haystack = new int[]{1, 2, 3};
+        needle = new int[]{};
+
+        assertEquals(Integer.MIN_VALUE, App.findPosition(haystack, needle));
+    }
+
+    @Test
+    public void itReturnsNegativeNumberWhenHaystackIsEmpty() throws Exception {
+        haystack = new int[]{};
+        needle = new int[]{1, 2, 3};
+
+        assertEquals(Integer.MIN_VALUE, App.findPosition(haystack, needle));
     }
 }
