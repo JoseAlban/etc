7f32fa517740181ee5143a56fb3f05b2d42f786a
Jose Alban <zebaum@gmail.com>
2015-09-07 00:00:08 +0100

    challenge1 - set up basic project

---
+++ challenges/challenge1/src/main/java/eu/ge0/App.java
@@ -0,0 +1,12 @@
+package eu.ge0;
+
+/**
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ challenges/challenge1/src/test/java/eu/ge0/AppTest.java
@@ -0,0 +1,12 @@
+package eu.ge0;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void itDoesThings() throws Exception {
+
+
+    }
+}
