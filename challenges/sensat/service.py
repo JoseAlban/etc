f7bd44a81a92d720deb47c4464aa2c2605e78fbe
Jose Alban <zebaum@gmail.com>
2019-10-22 23:51:32 +0100

    falcon via gunicorn serving skeleton

---
+++ challenges/sensat/service.py
@@ -0,0 +1,20 @@
+import falcon
+
+class QuoteResource:
+
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        quote = {
+            'quote': (
+                "I've always been more interested in "
+                "the future than in the past."
+            ),
+            'author': 'Grace Hopper'
+        }
+
+        resp.media = quote
+
+
+app = falcon.API()
+service = QuoteResource()
+app.add_route('/quote', service)
